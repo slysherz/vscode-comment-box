b53cbfd7522b791c19a5f35217aa3e53d9e5a02d
Diogo Ribeiro <diogo.ribeiro@fabamaq.com>
2018-02-14 14:21:31 +0000

    - Changed extension.js to use convertToCommentBox. - Restructured code to kick start multi-selection support.

--- src/extension.js
+++ src/extension.js
@@ -7,7 +7,8 @@ const vscode = require('vscode');
 const {
     maxWidth,
     padRight,
-    padToCenter
+    padToCenter,
+    convertToCommentBox
 } = require('./comment-box')
 
 
@@ -22,72 +23,55 @@ function activate(context) {
             // No open text editor
             return;
         }
-        
+
         // Load user settings
-        let configuration   = vscode.workspace.getConfiguration("commentBox")
-        let capitalize      = configuration.get("capitalize")
-        let extendSelection = configuration.get("extendSelection")
-        let startToken      = configuration.get("commentStartToken")
-        let endToken        = configuration.get("commentEndToken")
-        let lineStartToken  = configuration.get("leftEdgeToken")
-        let lineEndToken    = configuration.get("rightEdgeToken")
-        let fillingToken    = configuration.get("fillingToken")
-        let firstFillToken  = configuration.get("topEdgeToken")
-        let lastFillToken   = configuration.get("bottomEdgeToken")
-        let align           = configuration.get("textAlignment")
-        let clearAroundText = configuration.get("textToEdgeSpace")
-        
-        let selection = editor.selection;
-        
-        if (extendSelection) {
-            // Let's extend the selection from the first character of the first line
-            // to the last character of the last line
-            let last = editor.document.lineAt(selection.end.line).range.end.character
-            selection = new (vscode.Selection)(selection.start.line, 0, selection.end.line, last)
-        }
+        const configuration     = vscode.workspace.getConfiguration("commentBox")
+        const capitalize        = configuration.get("capitalize")
+        const extendSelection   = configuration.get("extendSelection")
+        const startToken        = configuration.get("commentStartToken")
+        const endToken          = configuration.get("commentEndToken")
+        const leftEdgeToken     = configuration.get("leftEdgeToken")
+        const rightEdgeToken    = configuration.get("rightEdgeToken")
+        const fillingToken      = configuration.get("fillingToken")
+        const topEdgeToken      = configuration.get("topEdgeToken")
+        const bottomEdgeToken   = configuration.get("bottomEdgeToken")
+        const align             = configuration.get("textAlignment")
+        const clearAroundText   = configuration.get("textToEdgeSpace")
+        const width             = configuration.get("boxWidth")
 
-        let text = document.getText(selection)
+        let editOperations = editor.selections.map((selection, selectionID) => {
+            if (extendSelection) {
+                // Let's extend the selection from the first character of the first line
+                // to the last character of the last line
+                let last = editor.document.lineAt(selection.end.line).range.end.character
+                selection = new(vscode.Selection)(selection.start.line, 0, selection.end.line, last)
+            }
+            
+            let text = document.getText(selection)
 
-        if (capitalize) text = text.toUpperCase()
+            if (capitalize) text = text.toUpperCase()
 
-        let lines = text.split(/\s*\n\s*/)
-        let maxLineWidth = maxWidth(lines)
-        
-        let edgesWidth = lineStartToken.length + lineEndToken.length
-        
-        // Calculate width of the box
-        let width = configuration.get("boxWidth") || maxLineWidth + edgesWidth + 2 * clearAroundText
-        let alignmentStyle = {
-            center: padToCenter,
-            left: padRight
-        }[align]
-        
-        lines = lines
-            // Make sure all lines have the same width
-            .map(line => alignmentStyle(line, maxLineWidth, " "))
+            text = convertToCommentBox(text, {
+                startToken,
+                endToken,
+                topEdgeToken,
+                bottomEdgeToken,
+                leftEdgeToken,
+                rightEdgeToken,
+                fillingToken,
+                width,
+                clearAroundText,
+                align,
+            });
 
-            // Add space to separate from edges
-            .map(line => padToCenter(line, maxLineWidth + 2 * clearAroundText, " "))
+            const resultLines = text.split(/\n/g)
+            const newLineSpan = resultLines.length - 1
+            const endLine = selection.start.line + newLineSpan
+            const finalAnchor = new(vscode.Position)(endLine, resultLines[newLineSpan].length)
 
-            // Extend lines to match desired with, using the choosen filling token
-            .map(line => padToCenter(line, width - edgesWidth, fillingToken))
-        
-        let lineFlip = lineEndToken + "\n" + lineStartToken
-
-        text = padRight(startToken, width, firstFillToken) + "\n" + 
-            lineStartToken + lines.join(lineFlip) + lineEndToken + "\n" + 
-            padRight(lineStartToken, width, lastFillToken) + endToken
-
-        let resultLines = text.split(/\n/g)
-        let newLineSpan = resultLines.length - 1
-        let endLine = selection.start.line + newLineSpan
-        let finalAnchor = new (vscode.Position)(endLine, resultLines[newLineSpan].length)
-
-        editor.edit(builder => {
-            builder.replace(selection, text);
-        })
-
-        editor.selection = new vscode.Selection(finalAnchor, finalAnchor)
+            editor.edit(builder => builder.replace(selection, text));
+            editor.selection[selectionID] = new vscode.Selection(finalAnchor, finalAnchor)
+        })        
     });
 
     context.subscriptions.push(commentBox);
